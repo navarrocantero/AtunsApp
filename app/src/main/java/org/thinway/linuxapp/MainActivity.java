916551b3969d0db3a8f89609eca0335da03ef5ad
Fran Delgado <fran@thinway.org>
2017-11-30 18:23:00 +0100

    Ahora la app procesa un Json y muestras los datos reflejados en él.

--- app/src/main/java/org/thinway/linuxapp/MainActivity.java
+++ app/src/main/java/org/thinway/linuxapp/MainActivity.java
@@ -1,19 +1,29 @@
 package org.thinway.linuxapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import org.thinway.linuxapp.model.Distro;
 
 import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView mListViewDistros;
-    ArrayList<String> mListaDistros = new ArrayList<>();
+    ArrayList<Distro> mListaDistros = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         mListViewDistros = findViewById(R.id.list_view_distros);
 
-        loadDistros();
-
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        final ArrayAdapter<Distro> adapter = new ArrayAdapter<Distro>(
                 this,
                 R.layout.list_item_distro,
                 R.id.text_view_distro,
@@ -36,69 +44,69 @@ public class MainActivity extends AppCompatActivity {
         mListViewDistros.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "Pulsado", Toast.LENGTH_SHORT).show();
-                //Intent detailIntent = new Intent(
-                //        MainActivity.this,
-                //        DetailActivity.class);
-                //detailIntent.putExtra("candy_name", mListaJedis.get(position));
-                //startActivity(detailIntent);
+                Intent detailIntent = new Intent(
+                        MainActivity.this,
+                        DetailActivity.class);
+                detailIntent.putExtra("distro_name", mListaDistros.get(position).name);
+                detailIntent.putExtra("distro_ostype", mListaDistros.get(position).ostype);
+                detailIntent.putExtra("distro_basedon", mListaDistros.get(position).basedon);
+                startActivity(detailIntent);
             }
         });
 
-        //AsyncHttpClient client = new AsyncHttpClient();
-//        client.get("https://vast-brushlands-23089.herokuapp.com/main/api",
-//                new TextHttpResponseHandler() {
-//                    @Override
-//                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-//                        Log.e("AsyncTask", "MUERTE: " + responseString);
-//                    }
-//
-//                    @Override
-//                    public void onSuccess(int statusCode, Header[] headers, String responseString) {
-//                        Log.d("AsyncTask", "Petición OK: " + responseString);
-//                        Gson gson = new GsonBuilder().create();
-//                        listaCandies = gson.fromJson(responseString, Candy[].class);
-//                        adapter.clear();
-//                        for(Candy candy: listaCandies){
-//                            adapter.add(candy.name);
-//                        }
-//                    }
-//                });
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://obscure-island-13934.herokuapp.com/api/distros",
+                new TextHttpResponseHandler() {
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        Log.e("MainActivity", "ADA sucks!!!");
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                        Log.d("MainActivity", "OK->Response:"+ responseString);
+                        Gson gson = new GsonBuilder().create();
+                        Distro[] distros = gson.fromJson(responseString, Distro[].class);
+                        adapter.clear();
+                        for (Distro distro: distros){
+                            adapter.add(distro);
+                        }
+                    }
+                });
     }
 
     private void loadDistros(){
-        mListaDistros.add("Luke");
-        mListaDistros.add("Rajivari†");
-        mListaDistros.add("Garon Jard");
-        mListaDistros.add("Cala Brin");
-        mListaDistros.add("Ters Sendon");
-        mListaDistros.add("Nomi Sunrider");
-        mListaDistros.add("Kavar†");
-        mListaDistros.add("Zez-Kai Ell†");
-        mListaDistros.add("Vrook Lamar†");
-        mListaDistros.add("Loona Vash†");
-        mListaDistros.add("Vandar Tokare†");
-        mListaDistros.add("Dorjander Kace");
-        mListaDistros.add("Lucien Draay");
-        mListaDistros.add("Mical");
-        mListaDistros.add("Belth Allusis†");
-        mListaDistros.add("Otegω");
-        mListaDistros.add("Nikil Nobil");
-        mListaDistros.add("Wens Aleusis");
-        mListaDistros.add("Oric Traless");
-        mListaDistros.add("Giffis Fane");
-        mListaDistros.add("Syo Bakarn");
-        mListaDistros.add("Jaric Kaedan†");
-        mListaDistros.add("Bela Kiwiiks");
-        mListaDistros.add("Orgus Din†");
-        mListaDistros.add("Tol Braga");
-        mListaDistros.add("Shol Bestros");
-        mListaDistros.add("Barsen'thor");
-        mListaDistros.add("Yaddle†");
-        mListaDistros.add("Oppo Rancisis†");
-        mListaDistros.add("Omo Bouri†");
-        mListaDistros.add("Tor Difusal");
-        mListaDistros.add("Thame Cerulian†");
-        mListaDistros.add("T'un");
+//        mListaDistros.add("Rajivari†");
+//        mListaDistros.add("Garon Jard");
+//        mListaDistros.add("Cala Brin");
+//        mListaDistros.add("Ters Sendon");
+//        mListaDistros.add("Nomi Sunrider");
+//        mListaDistros.add("Kavar†");
+//        mListaDistros.add("Zez-Kai Ell†");
+//        mListaDistros.add("Vrook Lamar†");
+//        mListaDistros.add("Loona Vash†");
+//        mListaDistros.add("Vandar Tokare†");
+//        mListaDistros.add("Dorjander Kace");
+//        mListaDistros.add("Lucien Draay");
+//        mListaDistros.add("Mical");
+//        mListaDistros.add("Belth Allusis†");
+//        mListaDistros.add("Otegω");
+//        mListaDistros.add("Nikil Nobil");
+//        mListaDistros.add("Wens Aleusis");
+//        mListaDistros.add("Oric Traless");
+//        mListaDistros.add("Giffis Fane");
+//        mListaDistros.add("Syo Bakarn");
+//        mListaDistros.add("Jaric Kaedan†");
+//        mListaDistros.add("Bela Kiwiiks");
+//        mListaDistros.add("Orgus Din†");
+//        mListaDistros.add("Tol Braga");
+//        mListaDistros.add("Shol Bestros");
+//        mListaDistros.add("Barsen'thor");
+//        mListaDistros.add("Yaddle†");
+//        mListaDistros.add("Oppo Rancisis†");
+//        mListaDistros.add("Omo Bouri†");
+//        mListaDistros.add("Tor Difusal");
+//        mListaDistros.add("Thame Cerulian†");
+//        mListaDistros.add("T'un");
     }
 }
