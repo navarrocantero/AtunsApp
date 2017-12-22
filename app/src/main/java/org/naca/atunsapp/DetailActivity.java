34ef1117bb21afb389e0a2d5f54e41b13a86d8c3
navarrocantero <navarrocantero@navarrocanteros-iMac.local>
2017-12-22 03:32:32 +0100

    tracks without rating fix

--- app/src/main/java/org/naca/atunsapp/DetailActivity.java
+++ app/src/main/java/org/naca/atunsapp/DetailActivity.java
@@ -26,55 +26,57 @@ import cz.msebera.android.httpclient.Header;
  */
 public class DetailActivity extends AppCompatActivity {
 
-  TextView mTextViewAlbumName;
-  TextView mTextViewAlbumArtist;
-  ImageView mImageViewAlbumImage;
-  ListView mListViewATracks;
-  ArrayList<Track> mlistTracks = new ArrayList<>();
+    TextView mTextViewAlbumName;
+    TextView mTextViewAlbumArtist;
+    ImageView mImageViewAlbumImage;
+    ListView mListViewATracks;
+    ArrayList<Track> mlistTracks = new ArrayList<>();
 
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(org.naca.atunsapp.R.layout.activity_detail);
-    Album album = new Album();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(org.naca.atunsapp.R.layout.activity_detail);
+        Album album = new Album();
 
-    mTextViewAlbumName = findViewById(R.id.textView_albumName);
-    mTextViewAlbumArtist = findViewById(R.id.textView_albumArtist);
-    mImageViewAlbumImage = findViewById(R.id.imageView_AlbumImage);
-    mListViewATracks = findViewById(R.id.list_view_tracks);
+        mTextViewAlbumName = findViewById(R.id.textView_albumName);
+        mTextViewAlbumArtist = findViewById(R.id.textView_albumArtist);
+        mImageViewAlbumImage = findViewById(R.id.imageView_AlbumImage);
+        mListViewATracks = findViewById(R.id.list_view_tracks);
 
-    Intent detailIntent = DetailActivity.this.getIntent();
-    if (detailIntent.hasExtra("album")) {
-      album = (Album) detailIntent.getSerializableExtra("album");
+        Intent detailIntent = DetailActivity.this.getIntent();
+        if (detailIntent.hasExtra("album")) {
+            album = (Album) detailIntent.getSerializableExtra("album");
+        }
+
+        mTextViewAlbumName.setText(album.getName());
+        mTextViewAlbumArtist.setText(album.getArtist());
+        
+        if (!album.getImage().equals("")) {
+            Picasso.with(this).load(album.getImage()).into(mImageViewAlbumImage);
+        }
+        final TrackAdapter trackAdapter = new TrackAdapter(this, mlistTracks);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(getString(R.string.URL) + getString(R.string.API) + album.name,
+                new TextHttpResponseHandler() {
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString,
+                                          Throwable throwable) {
+                        Log.e("MainActivity", "conection fail!!!");
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                        Log.d("MainActivity", "OK->Response:" + responseString);
+                        Gson gson = new GsonBuilder().create();
+                        Track[] tracks = gson.fromJson(responseString, Track[].class);
+                        for (Track track : tracks) {
+                            mlistTracks.add(track);
+                        }
+                        mListViewATracks.setAdapter(trackAdapter);
+
+                    }
+                });
     }
-
-    mTextViewAlbumName.setText(album.getName());
-    mTextViewAlbumArtist.setText(album.getArtist());
-    Picasso.with(this).load(album.getImage()).into(mImageViewAlbumImage);
-
-    final TrackAdapter trackAdapter = new TrackAdapter(this, mlistTracks);
-
-    AsyncHttpClient client = new AsyncHttpClient();
-    client.get(getString(R.string.URL) + getString(R.string.API) + album.name,
-        new TextHttpResponseHandler() {
-          @Override
-          public void onFailure(int statusCode, Header[] headers, String responseString,
-              Throwable throwable) {
-            Log.e("MainActivity", "conection fail!!!");
-          }
-
-          @Override
-          public void onSuccess(int statusCode, Header[] headers, String responseString) {
-            Log.d("MainActivity", "OK->Response:" + responseString);
-            Gson gson = new GsonBuilder().create();
-            Track[] tracks = gson.fromJson(responseString, Track[].class);
-            for (Track track : tracks) {
-              mlistTracks.add(track);
-            }
-            mListViewATracks.setAdapter(trackAdapter);
-
-          }
-        });
-  }
 }
--- app/src/main/java/org/naca/atunsapp/TrackAdapter.java
+++ app/src/main/java/org/naca/atunsapp/TrackAdapter.java
@@ -20,37 +20,40 @@ import java.util.ArrayList;
 public class TrackAdapter extends ArrayAdapter {
 
 
-  public TrackAdapter(@NonNull Context context, ArrayList<Track> arrayList) {
-    super(context, 0, arrayList);
-  }
-
-  @Override
-  public long getItemId(int position) {
-    return position;
-  }
-
-  @NonNull
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-
-    // Check if the current is empty
-    if (convertView == null) {
-      convertView = LayoutInflater.from(getContext()).inflate(
-          R.layout.list_item_track, parent, false);
+    public TrackAdapter(@NonNull Context context, ArrayList<Track> arrayList) {
+        super(context, 0, arrayList);
     }
-    Track track = (Track) getItem(position);
 
-    if (track != null) {
-      TextView name = (TextView) convertView.findViewById(R.id.text_view_track_name);
-      TextView bpm = (TextView) convertView.findViewById(R.id.text_view_track_bpm);
-      RatingBar ratingBar = (RatingBar) convertView.findViewById(R.id.ratingBar);
-
-      name.setText(track.getName());
-      bpm.setText(String.format("Bpm : %s", track.getBpm()));
-      try {
-        ratingBar.setRating(Float.parseFloat(track.getRating()));
-      }catch (NullPointerException e){}
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
-    return convertView;
-  }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Check if the current is empty
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item_track, parent, false);
+        }
+        Track track = (Track) getItem(position);
+
+        if (track != null) {
+            TextView name = (TextView) convertView.findViewById(R.id.text_view_track_name);
+            TextView bpm = (TextView) convertView.findViewById(R.id.text_view_track_bpm);
+            RatingBar ratingBar = (RatingBar) convertView.findViewById(R.id.ratingBar);
+
+            name.setText(track.getName());
+            bpm.setText(String.format("Bpm : %s", track.getBpm()));
+            try {
+                if (!track.getRating().trim().equals("")) {
+                    ratingBar.setRating(Float.parseFloat(track.getRating()));
+                }
+            } catch (NullPointerException e) {
+            }
+        }
+        return convertView;
+    }
 }
